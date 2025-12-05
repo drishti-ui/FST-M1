f141043e96906edec544535f5b20b86d55b34bfe
drishti-ui <drkumar1@in.ibm.com>
2025-12-05 16:01:31 +0530

    Add files via upload

---
+++ Python/Activities/Age.py
@@ -0,0 +1,8 @@
+current_year=2025
+
+name= input('Enter your name:')
+age = int(input('Enter your name:'))
+
+year_when_100= str(current_year+ (100-age))
+
+print(name +" , you will be 100 year in "+ year_when_100)
