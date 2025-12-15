7191227d1db14f15cb29ad3b04309a39e76eb7a3
drishti-ui <drkumar1@in.ibm.com>
2025-12-15 13:07:47 +0530

    Add files via upload

---
+++ Selenium/Activity1.java
@@ -0,0 +1,27 @@
+package activities;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity1 {
+	
+	public static void main(String[] args) {
+	
+	WebDriver driver = new FirefoxDriver();
+	
+	driver.get("https://training-support.net");
+	
+	String title = driver.getTitle();
+	
+	System.out.println("Page title is : " + title);
+	
+	driver.findElement(By.linkText("About Us")).click();
+	
+	System.out.println("New Page title is : " + driver.getTitle());
+	
+	driver.quit();
+	}
+
+}
---
+++ Selenium/Activity2.java
@@ -0,0 +1,28 @@
+
+package activities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Activity2 {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new FirefoxDriver();
+		
+		driver.get("https://training-support.net/webelements/login-form/");
+		
+		System.out.println("Page title is : " + driver.getTitle());
+		
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.id("password")).sendKeys("password");
+		driver.findElement(By.xpath("//button[text()='Submit']")).click();
+		
+		System.out.println("New Page title is : " + driver.findElement(By.tagName("h1")).getText());
+		
+		driver.quit();
+
+		}
+
+}
